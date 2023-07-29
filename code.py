5703493995fda816f335fd24857db083f2e79209
Katlego KGOSI <49514430+0092kat@users.noreply.github.com>
2023-07-29 19:24:24 +0200

    Update code.py, changed the hyperparameters

--- code.py
+++ code.py
@@ -30,7 +30,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=0)
 
 # Creating the RandomForestRegressor model and fitting the data
-model = RandomForestRegressor(random_state=0)
+model = RandomForestRegressor(n_estimators=100, learning_rate=0.1, random_state=0)
 
 model.fit(X_train, y_train)
 
